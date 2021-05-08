7e1ce6a76dd1bf377e82ce0fcdb150400b4abbd8
Sakshi-Saraswat <sakshisaraswat159@gmail.com>
2021-05-08 16:37:57 +0530

    update ToDoLst

--- src/ToDoLst.js
+++ src/ToDoLst.js
@@ -13,12 +13,7 @@ const ToDoLst=(props)=>{
         <li className="set" style={{textDecoration: clicked ? "line-through" : "none"}}>
         {props.text}</li>
         <i class="fa fa-thumbs-up" aria-hidden="true" style={{visibility : clicked ? "visible":"hidden"}}></i>
-        <i className="fa fa-thumbs-down" aria-hidden="true" style={{visibility : clicked ? "hidden":"visible"}}></i>
-        
-       
-       
-       
-        
+        <i className="fa fa-thumbs-down" aria-hidden="true" style={{visibility : clicked ? "hidden":"visible"}}></i>    
     </div>
     </> 
     );
